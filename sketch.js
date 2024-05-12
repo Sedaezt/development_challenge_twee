1013caa9d972e8152e1ac49699be64f9fb20fe3a
Sedaezt <sedaezzat@icloud.com>
2024-05-12 16:03:44 +0200

    vierde commit

--- sketch.js
+++ sketch.js
@@ -8,9 +8,14 @@ let startTime;  // timer
 let maxTime = 30; // tijd
 let handDetected = false;
 let gameEnded = false;
+let canvasWidth = 900;
+let canvasHeight = 900;
+let removedCircles = [];
+
 
 function setup() {
-  createCanvas(640, 480);
+  let canvas = createCanvas(canvasWidth, canvasHeight);
+  canvas.position((windowWidth - canvasWidth) / 2, (windowHeight - canvasHeight) / 2);
   video = createCapture(VIDEO); //webcam
   video.hide();     //video verbergen
   handpose = ml5.handpose(video, modelReady);    // roep modelready als de functie klaar is
@@ -21,42 +26,75 @@ function modelReady() {
     console.log("Model ready!");  // weten wnr model ready is 
   }
 
-function gotHands(results) {
-  predictions = results;
-  if (predictions.length > 0 && !handDetected) {
-    handDetected = true;
-    startTime = millis();   // timer starten bij detectie van hand
+  function gotHands(results) {
+    predictions = results;
+    if (predictions.length > 0 && !handDetected) {
+      handDetected = true;
+      startTime = millis();
+    }
+  
+    if (handDetected && circles.length > 0) {
+      let landmarks = predictions[0].landmarks; // kenmerken van de gedetecteerde hand opslagen
+      let handX = 0;
+      let handY = 0;
+      for (let j = 0; j < landmarks.length; j++) {
+        handX += landmarks[j][0];
+        handY += landmarks[j][1];
+      }
+      // gemiddelde x en y coördinaat berekenen
+      handX /= landmarks.length;
+      handY /= landmarks.length;
+  
+      
+      for (let j = circles.length - 1; j >= 0; j--) {
+        // midden van de hand en het midden van elk cirkel berekenen
+        let d = dist(handX, handY, circles[j].x, circles[j].y);
+        if (d < 25) { // afstand kleiner dan 25px
+          removedCircles.push(circles.splice(j, 1)[0]); 
+          circleSpeeds.splice(j, 1);
+          numRemovedCircles++;  // aantal verwijderde cirkels verhogen
+          if (numRemovedCircles >= 2) {
+            handDetected = false;
+            break;
+          }
+        }
+      }
+    }
   }
-}
 
 function draw() {
   if (handDetected && !gameEnded) { //handdetectie + spel niet geëindigd
     let elapsedTime = (millis() - startTime) / 1000;    //omzetten naar sec.
     // gameover als de tijd 0 is
-    if (elapsedTime >= maxTime) {
-      gameOver(); 
-    // spel gaat door  
+    if (numRemovedCircles >= 5) {
+      youWon();
+    } else if (elapsedTime >= maxTime) {
+      gameOver();
+    } else if (numRemovedCircles >= 2) {
+      youWon();
     } else {
       image(video, 0, 0, width, height);
       drawCircles();
       moveCircles();
-      removeCirclesOnHand();
+      removeCirclesOnHand(); // We moeten de functie hier aanroepen
       displayTime(elapsedTime);
       displayScore();
     }
   } else {
     background(0);
     fill(255);
-    textSize(32);
+    textSize(24);
     textAlign(CENTER, CENTER);
+    let textX = canvasWidth / 2;
+    let textY = canvasHeight / 2;
     if (gameEnded) {
-      if (numRemovedCircles >= 5) { // meer dan 5 cirkels verwijdert
-        text("You won!", width / 2, height / 2);
+      if (numRemovedCircles >= 5) {
+        text("You won!", textX, textY);
       } else {
-        text("Game Over", width / 2, height / 2);
+        text("Game Over", textX, textY);
       }
     } else {
-      text("Please show your hand to start", width / 2, height / 2);
+      text("Please show your hand to start", textX, textY);
     }
   }
 }
@@ -79,8 +117,9 @@ function drawCircles() {
 function moveCircles() {
   for (let i = 0; i < circles.length; i++) {
     circles[i].y += circleSpeeds[i]; // Verhoog y-positie
-    circle(circles[i].x, circles[i].y, 50);
-    if (circles[i].y > height + 50) { // Als cirkel onderaan is, opnieuw beginnen
+    let circleSize = random(30, 60);
+    circle(circles[i].x, circles[i].y, circleSize);
+    if (circles[i].y > height + 50) {
       circles[i].y = random(-100, -50);
     }
   }
@@ -89,14 +128,17 @@ function moveCircles() {
 function removeCirclesOnHand() {
   for (let i = 0; i < predictions.length; i++) {
     let landmarks = predictions[i].landmarks;
-    let tipIndex = 8; // wijsvinger
-    let tip = landmarks[tipIndex];
-    let tipX = tip[0];
-    let tipY = tip[1];
-    for (let j = circles.length - 1; j >= 0; j--) { 
-      let d = dist(tipX, tipY, circles[j].x, circles[j].y);
-      if (d < 25) {// Als de wijsvinger dichtbij de cirkel is
-        // Verwijder de cirkel en bijbehorende snelheid
+    let handX = 0;
+    let handY = 0;
+    for (let j = 0; j < landmarks.length; j++) {
+      handX += landmarks[j][0];
+      handY += landmarks[j][1];
+    }
+    handX /= landmarks.length;
+    handY /= landmarks.length;
+    for (let j = circles.length - 1; j >= 0; j--) {
+      let d = dist(handX, handY, circles[j].x, circles[j].y);
+      if (d < 25) {
         circles.splice(j, 1);
         circleSpeeds.splice(j, 1);
         numRemovedCircles++;
@@ -108,7 +150,7 @@ function removeCirclesOnHand() {
 // tijd
 function displayTime(time) {
   fill(255);
-  textSize(20);
+  textSize(18);
   textAlign(LEFT, TOP);
   text("Time: " + nf(maxTime - time, 0, 1), 10, 10); // Toon de resterende tijd
 }
@@ -116,7 +158,7 @@ function displayTime(time) {
 // score laten zien 
 function displayScore() {
   fill(255);
-  textSize(20);
+  textSize(18);
   textAlign(RIGHT, TOP);
   text("Score: " + numRemovedCircles, width - 10, 10); // Toon het aantal verwijderde cirkels
 }
